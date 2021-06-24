9898f3cd279cab4f3af26e47ae44f2dfa993429d
ChrisH2727 <chris@hullbrook.net>
2021-06-24 16:39:03 +0000

    Add register page

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
   $('.sidenav').sidenav();
   $(".dropdown-trigger").dropdown();
+  $('.datepicker').datepicker();
+  var now = new Date();
+  $("#activity_now_date").innerText = now.toLocaleString();
+
 });
 
 var TGSort = window.TGSort || function (n) {
