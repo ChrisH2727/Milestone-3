b607da04b3c5d7a54afe913b6d982d1c1a74a0cd
ChrisH2727 <chris@hullbrook.net>
2021-08-22 21:26:14 +0000

    Debug to False

--- app.py
+++ app.py
@@ -1231,4 +1231,4 @@ def faculty_link():
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
